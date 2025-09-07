0c9455bebdc0846d2d8f82d293b472221d5c355c
Dans <daniel_matar@outlook.com>
2025-09-07 22:26:21 +0300

    solving

--- inprogress/test.cpp
+++ inprogress/test.cpp
@@ -16,12 +16,6 @@ using namespace std;
 #define all(x) (x).begin(), (x).end()
 #define rall(x) (x).rbegin(), (x).rend()
 
-
-#define rep(i,a,b) for(int i = (a); i < (b); ++i)
-#define per(i,a,b) for(int i = (a); i >= (b); --i)
-#define rep_step(i, a, b, step) for(int i = (a); i < (b); i += (step))
-#define per_step(i, a, b, step) for(int i = (a); i >= (b); i -= (step))
-
 #define fastio() ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
 
 const ll INF = 1e18;
@@ -30,21 +24,50 @@ const int N = 2e5+5; // Maximum size for arrays
 
 void solve()
 {
-   
+   int m;
+   cin >>m;
+   int a[m] ,b[m];
+    for(int j=0;j<m;j++)
+    {
+        cin>>a[j];
+    }
+     for(int j=0;j<m;j++)
+    {
+        cin>>b[j];
+    }
+    int n = sizeof(a)/sizeof(a[0]);
+     sort(a, a + n);    
+    n = sizeof(b)/sizeof(b[0]);
+     sort(b, b + n); 
+  int counter =0;
+  int counter1 =0;
+    for(int i=0;i<m;i++)
+    {
+        for(int j=0;j<m;j++)
+        {
+            if(b[j]<a[i])
+            ;
+            else
+            counter+= (b[j]-a[i]);
+        }
+    }
 
+    cout<<"\n";
+    cout<<counter<<"\t"<<counter1;
+
+
+
+     return ;
 }
 
 
 int main() {
     fastio();
-
     int t = 1;
     cin >> t;
 
     while(t--) {
         solve();
     }
-
-
     return 0;
 }
